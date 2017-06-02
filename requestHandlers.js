c98f5b6bf1fe8d0f5febda7897198a1f36b2fdd2
yuan hehe <helpzp2008@126.com>
2017-06-02 16:18:03 +0800

    post 请求test

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,23 +1,21 @@
 var exec = require('child_process').exec;
 
-
 function start(response) {
     // console.log('Request handler start was called');
-    var content = 'empty';
-    exec('find /', function(error, stdout, stderr){
-        response.writeHead(200, {
-            "Content-Type": "text/plain"
-        });
-        response.write(stdout);
+    var body = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /' +
+            '></head><body><form action="/upload" method="post"><textarea name="text" rows="2' +
+            '0" cols="60"></textarea><input type="submit" value="Submit text" /></form></body' +
+            '></html>';
+    // exec('ls -lah', function (error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(body);
         response.end();
-    });
+    // });
 }
 
 function upload(response) {
     // console.log('Request handler upload was called');
-    response.writeHead(200, {
-            "Content-Type": "text/plain"
-        });
+    response.writeHead(200, {"Content-Type": "text/plain"});
     response.write('Hello upload');
     response.end();
 }
