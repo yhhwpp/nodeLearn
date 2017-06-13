f6e5dd3f78f9d13df913ffb9ff8b5883e0805f14
yuan hehe <helpzp2008@126.com>
2017-06-13 10:38:12 +0800

    express demo

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-var eventproxy = require('eventproxy');
-var superagent = require('superagent');
-var cheerio = require('cheerio');
-var url = require('url');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var cnodeUrl = 'https://cnodejs.org/';
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-superagent.get(cnodeUrl).end(function(err, res) {
-    if (err) {
-        return console.error(err);
-    }
-    var topicUrls = [];
-    var $ = cheerio.load(res.text);
-    $('#topic_list .topic_title').each(function(idx, element) {
-        var $element = $(element);
-        var href = url.resolve(cnodeUrl, $element.attr('href'));
-        topicUrls.push(href);
-    });
+var app = express();
 
-    var ep = new eventproxy();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-    ep.after('topic_html', topicUrls.length, function(topics) {
-        topics = topics.map(function(topicPair) {
-            var topicUrl = topicPair[0];
-            var topicHtml = topicPair[1];
-            var $ = cheerio.load(topicHtml);
-            return {
-                title: $('.topic_full_title').text().trim(),
-                href: topicUrl,
-                comment1: $('.reply_content').eq(0).text().trim()
-            };
-        });
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-        console.log('final:');
-        console.log(topics);
-    });
+app.use('/', index);
+app.use('/users', users);
 
-    topicUrls.forEach(function(topicUrl) {
-        superagent.get(topicUrl).end(function(err, res) {
-            console.log('fetch ' + topicUrl + ' successful');
-            ep.emit('topic_html', [topicUrl, res.text]);
-        });
-    });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
